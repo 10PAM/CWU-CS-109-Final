073b43d4222e815ee23a1fe8da72dde2d5605b67
MAP01 <167375195+10PAM@users.noreply.github.com>
2025-10-29 13:13:33 -0700

    Define algorithmic variables and common illnesses
    Added algorithmic variables and common illnesses with symptoms.

--- final.py
+++ final.py
@@ -1,3 +1,23 @@
-# Authors: Mario Aguilera; 
-# Date: 10/29/2025
-# File: final.py
+# Author(s): Mario Aguilera; 
+
+# Pre-define algorithmic variables
+current_score = 0
+# For symptoms, in each include 1 specific unique symptom to each if possible,
+# if not, 1 specific system unique to few to allow for easier determination
+common_illnesses = ("Common Cold", "Influenza", "Stomach Flu", "Coronavirus", "Pnuemonia")
+common_illnesses_symptoms = ["Coughing", "Sweating", ""]
+
+# Import our algo AI algorithm
+with open("algo_AI.txt", "a+") as algo_ai:
+    
+    found_sicknesses = -1
+    for line in algo_ai:
+        if line.substr(0, 9) == "sicknesses":
+            print(line)
+            found_sicknesses = line
+        
+        # Initialize algo_AI
+        if(found_sicknesses == -1):
+            for illness in common_illnesses:
+                print(illness)
+                #line.append("sicknesses: " + common_illnesses[illness] + "\n")
