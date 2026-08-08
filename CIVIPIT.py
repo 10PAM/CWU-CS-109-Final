e24032fab7ce8b2ada2605e6453ea18c67db52e4
MAP01 <maguilera131113@gmail.com>
2026-08-08 09:40:08 -0700

    Update author information in CIVIPIT.py
    Removed additional authors from the header comment.

--- CIVIPIT.py
+++ CIVIPIT.py
@@ -1,4 +1,4 @@
-# Author(s): Mario Aguilera; Jakob Greene; Jacky Njoroge
+# Author(s): Mario Aguilera
 # Date: 11/15/2026
 # File: CIVIPIT.py
 # About: Program that screens a user for 5 of the most commo viral infections in the US (Per the CDC)
